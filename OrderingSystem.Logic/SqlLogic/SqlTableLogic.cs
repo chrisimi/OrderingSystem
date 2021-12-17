efd8f06e00907f1f2d79d6461d246a0e57058181
PhilipGeier <philipgeieru@gmail.com>
2021-12-17 04:23:39 +0100

    Added SqlTableLogic.cs

---
+++ OrderingSystem.Logic/SqlLogic/SqlTableLogic.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using OrderingSystem.Domain;
+using OrderingSystem.Logic.Interfaces;
+
+namespace OrderingSystem.Logic.SqlLogic
+{
+    public class SqlTableLogic : ITableLogic
+    {
+        public void Add(Table table)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Edit(Table table)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Table table)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddDrinkToTable(Drink drink)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteDrinkFromTable(Drink drink)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Table GetTable(Guid guid)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Table> GetTables()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Drink> GetDrinks()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
