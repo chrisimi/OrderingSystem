232a41cb6b02b19be4693f76f3993b33731b2387
PhilipGeier <philipgeieru@gmail.com>
2021-12-17 03:59:00 +0100

    Completed Database Operations for Drinks

--- OrderingSystem.Logic/SqlLogic/SqlDrinkLogic.cs
+++ OrderingSystem.Logic/SqlLogic/SqlDrinkLogic.cs
@@ -1,24 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data;
 using MySql.Data.MySqlClient;
 using OrderingSystem.Domain;
 using OrderingSystem.Logic.Interfaces;
+using Org.BouncyCastle.Security;
 
 namespace OrderingSystem.Logic.SqlLogic
 {
     public class SqlDrinkLogic : IDrinkLogic
     {
-        private DBConnection _dbConnection = DBConnection.Instance();
-        
-        
+        private readonly DBConnection _dbConnection = DBConnection.Instance();
+
         public void Add(Drink drink)
         {
             if (_dbConnection.IsConnected())
             {
-                const string query = "INSERT INTO drinks(id, name, price, ingredients, extra_info) VALUES(@id, @name, @price, @ingredients, @extra_info)";
-                
-                MySqlCommand command = new MySqlCommand(query, _dbConnection.Connection);
+                const string query =
+                    "INSERT INTO drinks(id, name, price, ingredients, extra_info) VALUES(@id, @name, @price, @ingredients, @extra_info)";
+
+                var command = new MySqlCommand(query, _dbConnection.Connection);
                 command.Parameters.AddWithValue("@id", drink.Id);
                 command.Parameters.AddWithValue("@name", drink.Name);
                 command.Parameters.AddWithValue("@price", drink.Price);
@@ -31,22 +31,96 @@ namespace OrderingSystem.Logic.SqlLogic
 
         public void Edit(Drink drink)
         {
-            throw new NotImplementedException();
+            if (_dbConnection.IsConnected())
+            {
+                const string query =
+                    "UPDATE drinks SET id=@id, name=@name, price=@price, ingredients=@ingredients, extra_info=@extra_info WHERE id=@id";
+
+                var command = new MySqlCommand(query, _dbConnection.Connection);
+                command.Parameters.AddWithValue("@id", drink.Id);
+                command.Parameters.AddWithValue("@name", drink.Name);
+                command.Parameters.AddWithValue("@price", drink.Price);
+                command.Parameters.AddWithValue("@ingredients", drink.Ingredients);
+                command.Parameters.AddWithValue("@extra_info", drink.ExtraInfo);
+
+                command.ExecuteNonQuery();
+            }
         }
 
         public void Delete(Drink drink)
         {
-            throw new NotImplementedException();
+            if (_dbConnection.IsConnected())
+            {
+                const string query = "DELETE FROM drinks WHERE id=@id";
+
+                var command = new MySqlCommand(query, _dbConnection.Connection);
+                command.Parameters.AddWithValue("@id", drink.Id);
+
+                command.ExecuteNonQuery();
+            }
         }
 
         public Drink GetDrink(Guid guid)
         {
-            throw new NotImplementedException();
+            if (_dbConnection.IsConnected())
+            {
+                const string query = "SELECT * FROM drinks WHERE id=@id";
+
+                var command = new MySqlCommand(query, _dbConnection.Connection);
+                command.Parameters.AddWithValue("@id", guid);
+                
+                
+                using MySqlDataReader reader = command.ExecuteReader();
+
+                if (reader.HasRows)
+                {
+                    reader.Read();
+                    return new Drink()
+                    {
+                        Id = Guid.Parse(reader["id"].ToString()),
+                        Name = reader["name"].ToString(),
+                        Price = float.Parse(reader["price"].ToString()),
+                        Ingredients = reader["ingredients"].ToString(),
+                        ExtraInfo = reader["extra_info"].ToString()
+                    };
+                }
+                    
+            }
+
+            return null;
         }
 
         public List<Drink> GetDrinks()
         {
-            throw new NotImplementedException();
+            if (_dbConnection.IsConnected())
+            {
+                var returnList = new List<Drink>();
+
+                const string query = "SELECT * FROM drinks";
+
+                var command = new MySqlCommand(query, _dbConnection.Connection);
+
+                using MySqlDataReader reader = command.ExecuteReader();
+
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        returnList.Add(new Drink()
+                        {
+                            Id = Guid.Parse(reader["id"].ToString()),
+                            Name = reader["name"].ToString(),
+                            Price = float.Parse(reader["price"].ToString()),
+                            Ingredients = reader["ingredients"].ToString(),
+                            ExtraInfo = reader["extra_info"].ToString()
+                        });
+                    }
+
+                    return returnList;
+                }
+            }
+            return null;
         }
     }
 }
+        
